7a392feb729762b40a131d2d1b94e27c433489cc
Dominik <Dominik.apel00@gmail.com>
2023-03-09 16:51:30 +0100

    some stuff

--- webdini/pages/_document.js
+++ webdini/pages/_document.js
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import {Head, Html, Main, NextScript} from 'next/document'
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body style={{margin:0}}>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+    return (
+        <Html lang="en">
+            <Head>
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400..700&display=swap"
+                    rel="stylesheet"
+                />
+            </Head>
+            <body style={{margin: 0}}>
+            <Main/>
+            <NextScript/>
+            </body>
+        </Html>
+    )
 }
--- webdini/pages/index.js
+++ webdini/pages/index.js
@@ -1,21 +1,39 @@
-import { Button, Card, Layout } from "antd";
+import {Button, Card, Image} from "antd";
+import styles from '../styles/Home.module.css'
 
 export default function Home() {
-  return (
-    <div
-      style={{
-        height: "100vh",
-        backgroundColor: "#141414",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Button href="/test_page">Middle</Button>
-    </div>
-  );
+    return (
+        <div
+            style={{
+                height: "100vh",
+                width: "100vw",
+                backgroundColor: "#141414",
+                display: 'flex',
+                flexWrap: 'wrap',
+                justifyContent: 'center',
+                alignItems:'center',
+            }}
+        >
+                <div style={{marginLeft:'10rem', marginRight:'10rem', display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+                    <p className={styles.StartPageFontHeader}>Welcome to dini's Portfolio</p>
+                    <Button className={styles.StartPageFont} type={'link'}>Click here to get started.</Button>
+                </div>
+                <div style={{marginTop:'10rem',marginLeft:'10rem', marginRight:'10rem',display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
+                    <Card style={{paddingBottom:'20px', borderRadius: 1, transform: "rotate(3deg)"}}>
+                        <div style={{
+                            height: 450,
+                            width: 450,
+                            overflow: "hidden"
+                        }}>
+                            <Image src={"me01.jpg"} preview={false}/>
+                        </div>
+                    </Card>
+                    <p className={styles.StartPageFont}>This is me.</p>
+                </div>
+        </div>
+    );
 }
 
 Home.getLayout = (page) => {
-  return <>{page}</>;
+    return <main>{page}</main>;
 };
